45a55f3146699e1cb7cad7179a34b2f85ab1b001
gcaguilar <38892827+gcaguilar@users.noreply.github.com>
2026-03-16 12:30:36 +0100

    security: add COEP header for better resource isolation

--- next.config.ts
+++ next.config.ts
@@ -20,6 +20,10 @@ const securityHeaders = [
     ].join('; '),
   },
   {
+    key: 'Cross-Origin-Embedder-Policy',
+    value: 'require-corp',
+  },
+  {
     key: 'Cross-Origin-Opener-Policy',
     value: 'same-origin',
   },
